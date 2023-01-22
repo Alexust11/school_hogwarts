9dbedd60ce3e2b9c2e3e178fafff6478c879983f
user <alexust11@yandex.ru>
2023-01-22 16:07:43 +0700

    правка без oneToMany

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -4,17 +4,18 @@ package ru.hogwarts.school.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 @Entity
 public class Faculty {
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String color;
 
-
+//   private Student student;
     public Faculty(Long id, String name, String color) {
         this.id = id;
         this.name = name;
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -2,17 +2,18 @@ package ru.hogwarts.school.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 @Entity
 public class Student {
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int age;
 
-
+//  private Faculty faculty;
     public Student(Long id, String name, int age) {
         this.id = id;
         this.name = name;
