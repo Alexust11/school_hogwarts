f78f8f68a559e74e607565cd5623d34e0cb22462
Alexust11 <110272504+Alexust11@users.noreply.github.com>
2023-02-04 17:24:03 +0700

    Д/3 к уроку 4.2 с исправлениями

--- scripts422.sql
+++ scripts422.sql
@@ -1,23 +1,26 @@
 
 CREATE TABLE persons (
-                         person_id SERIAL PRIMARY KEY,
+                         id SERIAL PRIMARY KEY,
                          name VARCHAR(40) NOT NULL,
                          age INTEGER,
                          have_driver_license BOOLEAN DEFAULT FALSE,
-                         car_id INTEGER NOT NULL
+                         car_id INTEGER  REFERENCES cars (id)
 );
+
 
 ALTER TABLE persons
     ADD CONSTRAINT age_constraint CHECK (age >= 0);
 
 CREATE TABLE cars (
-                      car_id SERIAL PRIMARY KEY,
+                      id SERIAL PRIMARY KEY,
                       brand VARCHAR(40) NOT NULL,
                       model VARCHAR(40) NOT NULL,
                       cost MONEY NOT NULL
 );
+
 ALTER TABLE cars
     ADD CONSTRAINT cars_unique UNIQUE (brand, model);
+
 
 SELECT s.name, s.age, f.name
 FROM student s
